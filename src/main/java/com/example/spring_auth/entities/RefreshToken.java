c15c46989f50a47cdffa3980372694b92cb69536
Mohammad zaid Shaikh <109480030+Zaidshaikh2811@users.noreply.github.com>
2025-06-18 22:14:22 +0530

    first commit

---
+++ src/main/java/com/example/spring_auth/entities/RefreshToken.java
@@ -0,0 +1,4 @@
+package com.example.spring_auth.entities;
+
+public class RefreshToken {
+}
---
+++ src/main/java/com/example/spring_auth/entities/UserInfo.java
@@ -0,0 +1,34 @@
+package com.example.spring_auth.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table
+public class UserInfo {
+
+@Id
+@Column(name = "user_id")
+private long userId;
+
+
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name="user_id"),
+            inverseJoinColumns= @JoinColumn(name = "role_id")
+    )
+
+    private Set<UserRole> roles =new HashSet<>();
+
+}
